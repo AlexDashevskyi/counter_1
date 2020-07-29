5b6e54a8d1a07292d984b600b509b19c28d3c011
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-07-28 22:13:59 -0700

    counter_1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
 function App() {
+  const [value_1, setValue_1] = useState(0);
+  const [value_10, setValue_10] = useState(0);
+  const [valueRandom, setValueRandom] = useState(0)
+  const add_1 = () => {
+    setValue_1(value_1 + 1);
+  }
+  const add_10 = () => {
+    setValue_10(value_10 + 10);
+  };
+  const addRandom = () => {
+    setValueRandom(valueRandom + Math.floor((Math.random() * 10) + 1));
+  };
+  const minus_1 = () => {
+    setValue_1(value_1 - 1);
+  };
+  const minus_10 = () => {
+    setValue_10(value_10 - 10);
+  };
+  const minusRandom = () => {
+    setValueRandom(valueRandom - Math.floor((Math.random() * 10) + 1));
+  };
+  const reset = () => {
+    setValue_1(value_1 - value_1);
+    setValue_10(value_10 - value_10);
+    setValueRandom(valueRandom - valueRandom);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'App'}>
+        <header className={'App-value_1'}>COUNTERS</header>
+        <div className={'App-value_1'}>
+          <button onClick={add_1} className={'btn'}>+1</button>
+          {value_1}
+          <button onClick={minus_1} className={'btn'}>-1</button>
+        </div>
+        <div className={'App-value_2'}>
+          <button onClick={add_10} className={'btn'}>+10</button>
+          {value_10}
+          <button onClick={minus_10} className={'btn'}>-10</button>
+        </div>
+        <div className={'App-value_3'}>
+          <button onClick={addRandom} className={'btn'}>+Rand</button>
+          {valueRandom}
+          <button onClick={minusRandom} className={'btn'}>-Rand</button>
+        </div>
+        <div className={'App-value_4'}>
+          <button onClick={reset} className={'btn'}>Reset</button>
+        </div>
+      </div>
   );
 }
 
 export default App;
+
